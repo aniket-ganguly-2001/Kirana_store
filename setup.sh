39bb40364a6f8f984b2242e0a32c689a7fd400a5
Aniket Ganguly <aniket@aniket.Personal>
2024-01-03 13:05:46 +0530

    After disabling automatic deletion of the database file.

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 echo "=========================================================================================="
 echo "Welcome to the setup. This script creates a virtual environment .env in the current"
 echo "working directory, installs all the necessary packages into it, and runs the application."
-echo "The environment is deactivated and deleted and the cache files and the database file is "
-echo "deleted as soon as the server is stopped. Thus, this script can be run multiple times" 
-echo "without any conflict."
+echo "The environment is deactivated and deleted and the cache file is deleted as soon as the"
+echo "server is stopped. Thus, this script can be run multiple times without any conflict." 
 echo "=========================================================================================="
 echo "Creating environment..."
 python3 -m venv .env
@@ -22,8 +21,4 @@ rm -r .env
 if [ -e __pycache__ ]; then
     echo "Removing __pycache__ ..."  
     rm -r __pycache__
-fi
-if [ -e db_directory ]; then
-    echo "Removing database directory..."
-    rm -r "db_directory"
 fi
